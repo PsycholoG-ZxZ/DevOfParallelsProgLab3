5f0ecab2ad1f39d4e9eb7585de91a1ae48defa21
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-09 02:38:21 +0300

    Scripted auto-commit on change (2019-11-09 02:38:21) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
@@ -11,8 +11,8 @@ public class AirportsAndDelaysApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> Airports = sc.textFile("/home/dmitry/ParallLab3/DevOfParallelsProgLab3/MergeAirShowDelay/SMALL.csv");
         JavaRDD<String> Delays = sc.textFile("/home/dmitry/ParallLab3/DevOfParallelsProgLab3/MergeAirShowDelay/BIG.csv");
-        String AirString = Airports.toString();
-        String[] data = AirString.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+       // String AirString = Airports.toString();
+       // String[] data = AirString.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
         JavaRDD<String[]> RddArr = Airports.map(x -> x.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"));
     }
 
