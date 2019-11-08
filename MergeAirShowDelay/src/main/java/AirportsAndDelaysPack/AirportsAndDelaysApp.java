696266fcce0d6264f110c21877f263d6c6bb3d9e
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-09 02:49:26 +0300

    Scripted auto-commit on change (2019-11-09 02:49:26) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.api.java.JavaRDD;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 
 public class AirportsAndDelaysApp {
@@ -17,7 +18,7 @@ public class AirportsAndDelaysApp {
         JavaRDD<String[]> RddAirports = Airports.filter(str -> !str.startsWith("Code")).map(x -> x.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"));
         JavaRDD<String[]> RddDelays = Delays.filter(str -> !str.startsWith("\"YEAR\"")).map(x -> x.split(","));
 
-        JavaPairRDD<String, String> IdAndDescrPare = RddAirports.mapToPair(x -> return new Tuple )
+        JavaPairRDD<String, String> IdAndDescrPare = RddAirports.mapToPair(x -> return new Tuple2<> )
     }
 
 }
