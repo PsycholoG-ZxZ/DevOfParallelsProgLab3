eb23ba528651dda87aad7c8856346864c18af6d9
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-09 02:49:30 +0300

    Scripted auto-commit on change (2019-11-09 02:49:30) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
@@ -18,7 +18,7 @@ public class AirportsAndDelaysApp {
         JavaRDD<String[]> RddAirports = Airports.filter(str -> !str.startsWith("Code")).map(x -> x.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"));
         JavaRDD<String[]> RddDelays = Delays.filter(str -> !str.startsWith("\"YEAR\"")).map(x -> x.split(","));
 
-        JavaPairRDD<String, String> IdAndDescrPare = RddAirports.mapToPair(x -> return new Tuple2<> )
+        JavaPairRDD<String, String> IdAndDescrPare = RddAirports.mapToPair(x -> return new Tuple2<>() )
     }
 
 }
