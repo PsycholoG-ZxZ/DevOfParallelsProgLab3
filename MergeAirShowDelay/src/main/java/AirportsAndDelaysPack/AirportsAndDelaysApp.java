a586967653cb5f602f338757e4b80f6b41d3f0a3
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-09 01:36:25 +0300

    Scripted auto-commit on change (2019-11-09 01:36:25) by gitwatch.sh

---
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
@@ -0,0 +1,16 @@
+package AirportsAndDelaysPack;
+import org.apache.spark.api.java.JavaRDD;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+
+public class AirportsAndDelaysApp {
+    public static void main (String[] args){
+        SparkConf conf = new SparkConf().setAppName("lab5");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> Airports = sc.textFile("/home/dmitry/ParallLab3/DevOfParallelsProgLab3/MergeAirShowDelay/SMALL.csv");
+        JavaRDD<String> Delays = sc.textFile("/home/dmitry/ParallLab3/DevOfParallelsProgLab3/MergeAirShowDelay/BIG.csv");
+    }
+
+}
