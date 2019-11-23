dc24851c5f8a22ac589db96ae0e047d3c6fbaf04
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 05:08:42 +0300

    Scripted auto-commit on change (2019-11-23 05:08:42) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightData.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightData.java
@@ -34,7 +34,7 @@ public class FlightData implements Serializable {
         countAll = count;
         //canceledKol = countAll;
         if(kolFlightWDelay != 0){
-            per = float(kolFlightWDelay) * 100 /
+            per = float(kolFlightWDelay) * 100 /countAll;
         }
     }
 }
