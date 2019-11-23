3d4a395cbfd820721204be3023e9d8ed1db10a0a
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 05:20:11 +0300

    Scripted auto-commit on change (2019-11-23 05:20:11) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightsReduce.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightsReduce.java
@@ -27,7 +27,7 @@ public class FlightsReduce {
                 )
         ).mapToPair((
                 Tuple2<Tuple2<String,String>,String> tup)-> new Tuple2<>(
-                        tup._1, new Tuple2<>(tup._2.get
+                        tup._1, new Tuple2<>(tup._2)
                 )
         )
 
