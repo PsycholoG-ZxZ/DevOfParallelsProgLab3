b01bd2e92375bfb4f4311a333b59b23549ea6f46
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 04:20:01 +0300

    Scripted auto-commit on change (2019-11-23 04:20:01) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightsReduce.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/FlightsReduce.java
@@ -1,9 +1,12 @@
 package AirportsAndDelaysPack;
 
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
+
+import java.util.Map;
 
 public class FlightsReduce {
     public static JavaPairRDD<Tuple2, Tuple2> FlightsCount (JavaPairRDD<Tuple2<String,String>, FlightData> TupleWithKey,
-                                                            )
+                                                            Broadcast<Map<String,String>> AirBroadcast)
 }
