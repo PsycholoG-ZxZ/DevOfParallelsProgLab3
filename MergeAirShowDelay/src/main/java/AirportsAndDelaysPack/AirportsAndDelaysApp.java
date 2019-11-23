71dd82dcd278be9acc37eee8caf2859c46365802
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 05:21:50 +0300

    Scripted auto-commit on change (2019-11-23 05:21:50) by gitwatch.sh

--- MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
+++ MergeAirShowDelay/src/main/java/AirportsAndDelaysPack/AirportsAndDelaysApp.java
@@ -30,7 +30,7 @@ public class AirportsAndDelaysApp {
         JavaRDD<String> ExtraFlInfo;
         final Broadcast<Map<String,String>> AirBroadcast = sc.broadcast(airportMap);
         JavaPairRDD<Tuple2, Tuple2> Inf = FlightsReduce.FlightsCount(RddDelays, AirBroadcast);
-        Inf.saveAsTextFile();
+        Inf.saveAsTextFile("/home/dmitry/ParallLab3/DevOfParallelsProgLab3");
     }
 
 }
